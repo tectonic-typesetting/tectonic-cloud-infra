40e11e53c9e133d8816714c12979b6f90dd6c455
Peter Williams <peter@newton.cx>
2022-09-27 23:44:46 -0400

    azure/relay.tf: record Docker info

--- azure/relay.tf
+++ azure/relay.tf
@@ -22,7 +22,6 @@ resource "azurerm_linux_web_app" "relay" {
   location            = azurerm_resource_group.relay.location
   resource_group_name = azurerm_resource_group.relay.name
   service_plan_id     = azurerm_service_plan.assets.id
-  # Docker image: tectonictypesetting/relay-service:latest
 
   app_settings = {
     "DOCKER_ENABLE_CI"           = "true"
@@ -33,6 +32,11 @@ resource "azurerm_linux_web_app" "relay" {
     always_on         = true
     app_command_line  = ""
     use_32_bit_worker = false
+
+    application_stack {
+      docker_image     = "tectonictypesetting/relay-service"
+      docker_image_tag = "latest"
+    }
   }
 }
 
